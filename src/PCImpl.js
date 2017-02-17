2e5e9cd2a06e99d3ac41f37e65329a4ae52206b2
tonymiao2012 <miaozixiong@gmail.com>
2017-02-17 15:57:58 +0800

    Several updates about test cases.

--- src/PCImpl.js
+++ src/PCImpl.js
@@ -1,21 +1,38 @@
-function getPin()
-{
-	return model.parentlock.getPin()
+function startGetPin() {
+    var val = model.parentlock.getPin();
+    $("#details").html(val);
+    if (val.length != 0)
+        return true;
+    else
+        return false;
 }
-function setPin(pinValue) {
-    QUnit.test( "setPin test", function( assert ) {
-       model.parentlock.setPin(pinValue); 
-        assert.equal(getPin(),pinValue, "check setPin " );	
-    });   
+function getPin(funcName) {
+    QUnit.test(funcName, function (assert) {
+        var result = startGetPin();
+        assert.ok(result, "Test getPin");
+    });
+}
+function startSetPin(pinValue) {
+    model.parentlock.setPin(pinValue);
+    if (model.parentlock.getPin() == pinValue)
+        return true;
+    else
+        return false;
+}
+function setPin(pinValue, funcName) {
+    QUnit.test(funcName, function (assert) {
+        var result = startSetPin(pinValue);
+        assert.ok(result, "Test setPin");
+    });
 }
 
-function getMovieRating()
-{
-	return model.parentlock.getUsMovieRating()
+
+function getMovieRating() {
+    return model.parentlock.getUsMovieRating()
 }
 function setMovieRating(ratingValue) {
-    QUnit.test( "setUsMovieRating test", function( assert ) {
-       model.parentlock.setUsMovieRating(ratingValue); 
-        assert.equal(getMovieRating(),ratingValue, "check setUsMovieRating " );	
-    });   
+    QUnit.test("setUsMovieRating test", function (assert) {
+        model.parentlock.setUsMovieRating(ratingValue);
+        assert.equal(getMovieRating(), ratingValue, "check setUsMovieRating ");
+    });
 }
