4347ca725349461ea7a96a3a8a63fdd08448350e
tonymiao2012 <miaozixiong@gmail.com>
2017-05-09 20:25:36 +0800

    Prompt component.

--- component/prompt.js
+++ component/prompt.js
@@ -3,5 +3,15 @@
  */
 new Vue({
     el: "#prompt",
+    data: {
+        message: "",
+        icon: "fa fa-bomb fa-6",
+        cornerInfo: "Press 'OK' return",
+        hidden: false
+    }
+});
 
+Vue.component('prompt', {
+    props: info,
+    template: "<span></span><p>{{ info }}</p>"
 });
